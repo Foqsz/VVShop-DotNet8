cfa1a5e7827c00619c08b01ea6b14b38021c7a33
Victor Vinicius <victormafiavinicius@gmail.com>
2024-07-23 10:45:16 -0300

    Add project files.

---
+++ VVShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VVShop.ProductApi.Models;
+
+namespace VVShop.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+        
+        public DbSet<CategoryModel> Category { get; set; }
+        public DbSet<ProductModel> Product { get; set; } 
+    }
+}
---
+++ VVShop.ProductApi/Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VVShop.ProductApi.Models;
+
+public class CategoryModel
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+
+    public ICollection<ProductModel>? Products { get; set; }
+}
