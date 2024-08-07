456f75f5fc7b56f9c97256f63b4323e6e2dc3204
Foqsz <contatovictorvinicius05@gmail.com>
2024-08-07 09:02:53 -0300

    Criando o serviço de carrinho de compras. Banco de dados iniciado e arquivos de configuração.

---
+++ VVShop.CartApi/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VVShop.CartApi.Models;
+
+namespace VVShop.CartApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<CartItem> CartItems { get; set; } 
+    public DbSet<CartHeader> CartHeaders { get; set; }
+
+    //Fluent Api
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        //Product
+        mb.Entity<Product>().HasKey(c => c.Id);
+
+        //Product
+        mb.Entity<Product>().Property(c => c.Id).ValueGeneratedNever();
+
+        //Cart Header
+        mb.Entity<CartHeader>().Property(c => c.UserId).HasMaxLength(255).IsRequired();
+
+        mb.Entity<CartHeader>().Property(c => c.CouponCode).HasMaxLength(100);
+    }
+}
---
+++ VVShop.CartApi/Models/Cart.cs
@@ -0,0 +1,7 @@
+﻿namespace VVShop.CartApi.Models;
+
+public class Cart
+{
+    public CartHeader CartHeader { get; set; } = new CartHeader();
+    public IEnumerable<CartItem> cartItems { get; set; } = Enumerable.Empty<CartItem>();
+}
